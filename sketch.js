1112645dab56a23407a6944f801f3a1c3c1ef10f
EdvinVJouband <126730421+EdvinVJouband@users.noreply.github.com>
2023-04-24 14:32:57 -0600

    accidentely pressed close window

--- sketch.js
+++ sketch.js
@@ -1,15 +1,34 @@
-// Project Title
-// Your Name
-// Date
+// Pac Man Project
+// Edvin Jouband
+// Monday, April 24, 2023
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Cell {
+  constructor(i, j) {
+    this.x = i;
+    this.y = j;
+  }
+}
+
+let cellSize;
+const ROWS = 25, COLS = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createCellSize();
 }
 
 function draw() {
   background(220);
+}
+
+function createCellSize() {
+  if (width < height) {
+    cellSize = width/ROWS;
+  }
+  else {
+    cellSize = height/ROWS;
+  }
 }
